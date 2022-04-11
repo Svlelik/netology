f8f2716d0781af5b3b2ade76a4508f103a5c873f
Alex Svetlakov <lelikaas@gmail.com>
2022-04-11 19:33:31 +0300

    Add lesson one 1_1 file

---
+++ Introduction-to-Python/lesson_1_1.py
@@ -0,0 +1,58 @@
+print('hello "world"')
+print("hello 'world'")
+print('Привет', 'py', 44)
+
+
+str_1 = 'hello'
+str_2 = 'py-44'
+print(str_1 + str_2)
+
+print(str_1 * 5)
+
+print(str_1[0])
+print(str_1[1])
+print(str_1[:2])
+print(str_1[2:])
+print(str_1[1:3])
+print(str_1.capitalize())
+print(str_1.find('e'))
+
+#  '5'== 5
+# False
+#  int('5')
+# 5
+#  int('5')==5
+# True
+result = 2 + 2
+# print(result)
+
+# number1 = int(input('Первое число: '))
+# number2 = int(input('Второе число: '))
+# print(number1 - number2)
+
+
+number1 = 2
+number2 = 8
+result = number1 * number2
+print(result)
+
+print(2 ** 8)
+
+print(10 / 5)
+
+print(10 / 3)
+
+print(10 // 4)
+
+print(10 % 3)
+
+print((2 + 2) * 2)
+
+print(2 + 2 - 2 * 2 / 2)
+
+
+"""
+Это 
+многострочный 
+комментарий
+"""
